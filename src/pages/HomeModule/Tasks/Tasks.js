cc233fccf789b63162f747ca3d319c03e0b9de2a
Vlad Kravets <hazardvlad19@gmail.com>
2022-08-10 14:01:32 +0300

    created a taskbar tab to the sidebar and created a table with antdesign

---
+++ src/pages/HomeModule/Tasks/Tasks.js
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import TaskTable from "pages/HomeModule/Tasks/TasksTable";
+
+const Tasks = () => {
+    const [photos, setPhotos] = useState([]);
+
+    // Api
+    const instance = axios.create({
+        baseURL: 'https://picsum.photos/v2/'
+    })
+    const photosAPI = {
+        getPhotos() {
+            return instance.get('list?page=2&limit=5')
+        }
+    }
+
+    useEffect(() => {
+        photosAPI.getPhotos()
+            .then((res) => {
+                setPhotos(res.data)
+            })
+    }, [setPhotos])
+
+
+    return (
+        <>
+            <TaskTable/>
+
+        </>
+    );
+};
+
+export default Tasks;
